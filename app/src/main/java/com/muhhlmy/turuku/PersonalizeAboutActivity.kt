110deb22d1088b2cd0180a4c4d3b3968ceaafde3
muhhlmy <muhhlmy@gmail.com>
2024-11-21 14:49:17 +0700

    Fix Dropdown

--- app/src/main/java/com/muhhlmy/turuku/PersonalizeAboutActivity.kt
+++ app/src/main/java/com/muhhlmy/turuku/PersonalizeAboutActivity.kt
@@ -1,11 +1,23 @@
 package com.muhhlmy.turuku
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.muhhlmy.turuku.databinding.ActivityPersonalizeAboutBinding
 
 class PersonalizeAboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPersonalizeAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_personalize_about)
+        binding = ActivityPersonalizeAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val spinner : Spinner = binding.spinnerGender
+        val adapter = ArrayAdapter.createFromResource(this, R.array.gender_options,R.layout.spinner_item)
+        adapter.setDropDownViewResource(R.layout.spinner_item)
+        spinner.adapter = adapter
     }
 }
