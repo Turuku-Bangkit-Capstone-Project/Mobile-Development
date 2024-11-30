7b26df16474f49c9b28df80fe4c6580d4f289841
muhhlmy <muhhlmy@gmail.com>
2024-11-30 08:47:21 +0700

    Add Navbar and StaticsFragment & PreferencesFragment

---
+++ app/src/main/java/com/c242ps070/turuku/activity/SetAlarmActivity.kt
@@ -0,0 +1,12 @@
+package com.c242ps070.turuku.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.c242ps070.turuku.R
+
+class SetAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_alarm)
+    }
+}
--- app/src/main/java/com/c242ps070/turuku/dashboard_fragment/HomeFragment.kt
+++ app/src/main/java/com/c242ps070/turuku/dashboard_fragment/HomeFragment.kt
@@ -1,20 +1,33 @@
 package com.c242ps070.turuku.dashboard_fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.c242ps070.turuku.R
+import com.c242ps070.turuku.activity.SetAlarmActivity
+import com.c242ps070.turuku.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.sleepButton.setOnClickListener {
+            startActivity(Intent(context, SetAlarmActivity::class.java))
+        }
     }
 
 }
