08a990eca99d1dbaa0da323a2b84b1ee83b2dc34
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-03 20:09:14 +0700

    add function to login

--- app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
@@ -1,9 +1,15 @@
 package com.c242ps070.turuku.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.c242ps070.turuku.data.Result
+import com.c242ps070.turuku.data.remote.request.LoginRequest
+import com.c242ps070.turuku.data.remote.response.LoginResponse
 import com.c242ps070.turuku.data.repository.AuthRepository
 
 class LoginViewModel(
     private val authRepository: AuthRepository
 ): ViewModel() {
+    fun login(loginRequest: LoginRequest): LiveData<Result<LoginResponse>> =
+        authRepository.login(loginRequest)
 }
