b5c928a9a662bfbaebe962f99f157f3376c423c1
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-03 17:23:34 +0700

    update to set retrofit to consume api

--- app/src/main/java/com/c242ps070/turuku/data/remote/retrofit/ApiClient.kt
+++ app/src/main/java/com/c242ps070/turuku/data/remote/retrofit/ApiClient.kt
@@ -1,4 +1,34 @@
 package com.c242ps070.turuku.data.remote.retrofit
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiClient {
+    private const val BASE_URL = "https://prod-turuku-800638181621.asia-southeast2.run.app/"
+
+    fun create(token: String): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient(token))
+            .build()
+            .create(ApiService::class.java)
+    }
+
+    private fun okHttpClient(token: String): OkHttpClient {
+        val authInterceptor = Interceptor { chain ->
+            val req = chain.request()
+            val requestHeaders = req.newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(requestHeaders)
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .addInterceptor(authInterceptor)
+            .build()
+    }
 }
