09d9707d0667c1874d15e6300977ad2de0bd0554
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-03 18:28:41 +0700

    add auth repository to inject

--- app/src/main/java/com/c242ps070/turuku/data/di/Injection.kt
+++ app/src/main/java/com/c242ps070/turuku/data/di/Injection.kt
@@ -1,4 +1,19 @@
 package com.c242ps070.turuku.data.di
 
+import android.content.Context
+import com.c242ps070.turuku.data.local.datastore.UserPreference
+import com.c242ps070.turuku.data.local.datastore.dataStore
+import com.c242ps070.turuku.data.remote.retrofit.ApiClient
+import com.c242ps070.turuku.data.repository.AuthRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+
 object Injection {
+    fun provideAuthRepository(context: Context): AuthRepository {
+        val userPreference = UserPreference.getInstance(context.dataStore)
+        val apiService = ApiClient.create(
+            runBlocking { userPreference.getUser().first().token }
+        )
+        return AuthRepository(apiService)
+    }
 }
