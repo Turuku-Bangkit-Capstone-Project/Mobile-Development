796ce250f1056c2dce50aa951d72e10e0219f0dc
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-03 18:28:07 +0700

    add login, register, logout and refresh token api call

--- app/src/main/java/com/c242ps070/turuku/data/repository/AuthRepository.kt
+++ app/src/main/java/com/c242ps070/turuku/data/repository/AuthRepository.kt
@@ -1,4 +1,68 @@
 package com.c242ps070.turuku.data.repository
 
-class AuthRepository {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.c242ps070.turuku.data.remote.request.RegisterRequest
+import com.c242ps070.turuku.data.remote.retrofit.ApiService
+import com.google.gson.Gson
+import retrofit2.HttpException
+import com.c242ps070.turuku.data.Result
+import com.c242ps070.turuku.data.remote.request.LoginRequest
+import com.c242ps070.turuku.data.remote.response.ErrorResponse
+import com.c242ps070.turuku.data.remote.response.LoginResponse
+import com.c242ps070.turuku.data.remote.response.RegisterResponse
+
+class AuthRepository(
+    private val apiService: ApiService
+) {
+    fun register(registerRequest: RegisterRequest): LiveData<Result<RegisterResponse>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.register(registerRequest)
+            emit(Result.Success(response))
+        } catch (e: HttpException) {
+            val jsonInString = e.response()?.errorBody()?.string()
+            val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
+            val errorMessage = errorBody.message ?: ""
+            emit(Result.Error(errorMessage))
+        } catch (e: Exception) {
+            Log.d("AuthRepository", "register: ${e.message.toString()} ")
+            emit(Result.Error(e.message.toString()))
+        }
+    }
+
+    fun login(loginRequest: LoginRequest): LiveData<Result<LoginResponse>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.login(loginRequest)
+            emit(Result.Success(response))
+        } catch (e: HttpException) {
+            val jsonInString = e.response()?.errorBody()?.string()
+            val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
+            val errorMessage = errorBody.message ?: ""
+            emit(Result.Error(errorMessage))
+        } catch (e: Exception) {
+            Log.d("AuthRepository", "login: ${e.message.toString()} ")
+            emit(Result.Error(e.message.toString()))
+        }
+    }
+
+    suspend fun logout() = apiService.logout()
+
+    fun refreshToken(): LiveData<Result<LoginResponse>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.refreshToken()
+            emit(Result.Success(response))
+        } catch (e: HttpException) {
+            val jsonInString = e.response()?.errorBody()?.string()
+            val errorBody = Gson().fromJson(jsonInString, ErrorResponse::class.java)
+            val errorMessage = errorBody.message ?: ""
+            emit(Result.Error(errorMessage))
+        } catch (e: Exception) {
+            Log.d("AuthRepository", "refreshToken: ${e.message.toString()} ")
+            emit(Result.Error(e.message.toString()))
+        }
+    }
 }
