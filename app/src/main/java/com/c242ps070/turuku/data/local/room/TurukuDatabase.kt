518e7e58fa2c8af57d01741c3114043af08db176
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-04 14:57:20 +0700

    add sleep history dao

--- app/src/main/java/com/c242ps070/turuku/data/local/room/TurukuDatabase.kt
+++ app/src/main/java/com/c242ps070/turuku/data/local/room/TurukuDatabase.kt
@@ -1,6 +1,35 @@
 package com.c242ps070.turuku.data.local.room
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.c242ps070.turuku.data.local.room.dao.SleepHistoryDao
+import com.c242ps070.turuku.data.local.room.entity.SleepHistoryEntity
 
+@Database(
+    entities = [SleepHistoryEntity::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class TurukuDatabase: RoomDatabase() {
+    abstract fun sleepHistoryDao(): SleepHistoryDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TurukuDatabase? = null
+
+        @JvmStatic
+        fun getDatabase(context: Context): TurukuDatabase {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    TurukuDatabase::class.java, "turuku_db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { INSTANCE = it }
+            }
+        }
+    }
 }
