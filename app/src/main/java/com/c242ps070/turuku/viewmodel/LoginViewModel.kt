55e60863e0104d955b183515785025a5b296c8df
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-05 18:19:38 +0700

    add get refreshToken, get user, save userLoggedIn and get userLoggedIn

--- app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
@@ -2,14 +2,33 @@ package com.c242ps070.turuku.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.c242ps070.turuku.data.Result
+import com.c242ps070.turuku.data.local.datastore.UserPreference
+import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
 import com.c242ps070.turuku.data.remote.request.LoginRequest
 import com.c242ps070.turuku.data.remote.response.LoginResponse
+import com.c242ps070.turuku.data.remote.response.UserResponse
 import com.c242ps070.turuku.data.repository.AuthRepository
+import kotlinx.coroutines.launch
 
 class LoginViewModel(
-    private val authRepository: AuthRepository
+    private val authRepository: AuthRepository,
+    private val userPreference: UserPreference
 ): ViewModel() {
     fun login(loginRequest: LoginRequest): LiveData<Result<LoginResponse>> =
         authRepository.login(loginRequest)
+
+    fun refreshToken(): LiveData<Result<LoginResponse>> = authRepository.refreshToken()
+
+    fun getUser(): LiveData<Result<UserResponse>> = authRepository.getUser()
+
+    fun saveUserLoggedIn(userPreferenceModel: UserPreferenceModel) {
+        viewModelScope.launch {
+            userPreference.saveUser(userPreferenceModel)
+        }
+    }
+
+    fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
 }
