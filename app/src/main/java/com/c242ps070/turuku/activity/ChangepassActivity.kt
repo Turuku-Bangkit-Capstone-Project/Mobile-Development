c4e148686948a5d838c428ada283b1eef2b6a315
Rabsxd <reyanbz.rb@gmail.com>
2024-12-05 14:51:08 +0700

    changing AboutActivity.kt to DailyActivity.kt SecurityActivity.kt to ChangepassActivity.kt

--- app/src/main/java/com/c242ps070/turuku/activity/ChangepassActivity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/ChangepassActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.c242ps070.turuku.R
 
-class AboutActivity : AppCompatActivity() {
+class ChangepassActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_about)
+        setContentView(R.layout.activity_changepass)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/c242ps070/turuku/activity/DailyActivity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/DailyActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.c242ps070.turuku.R
 
-class SecurityActivity : AppCompatActivity() {
+class DailyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_security)
+        setContentView(R.layout.activity_daily)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
