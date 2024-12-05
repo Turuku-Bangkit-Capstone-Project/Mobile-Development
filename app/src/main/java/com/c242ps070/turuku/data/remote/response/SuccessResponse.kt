8472f41d26b8b2bdaa928273364ab8186e43e780
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-05 18:39:26 +0700

    add success response as general response that only message response

--- app/src/main/java/com/c242ps070/turuku/data/remote/response/SuccessResponse.kt
+++ app/src/main/java/com/c242ps070/turuku/data/remote/response/SuccessResponse.kt
@@ -2,7 +2,7 @@ package com.c242ps070.turuku.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
-data class RegisterResponse(
-    @field:SerializedName("msg")
-    val message: String,
+data class SuccessResponse(
+    @SerializedName("msg")
+    val message: String? = null
 )
