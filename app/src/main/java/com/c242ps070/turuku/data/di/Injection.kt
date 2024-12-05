c57f0965a8a3d9cf1f0471e2036ff2a041ef217d
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-05 18:53:31 +0700

    add user repository to inject

--- app/src/main/java/com/c242ps070/turuku/data/di/Injection.kt
+++ app/src/main/java/com/c242ps070/turuku/data/di/Injection.kt
@@ -5,6 +5,7 @@ import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.dataStore
 import com.c242ps070.turuku.data.remote.retrofit.ApiClient
 import com.c242ps070.turuku.data.repository.AuthRepository
+import com.c242ps070.turuku.data.repository.UserRepository
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
@@ -15,5 +16,13 @@ object Injection {
             runBlocking { userPreference.getUser().first().token }
         )
         return AuthRepository(apiService)
+    }
+
+    fun provideUserRepository(context: Context): UserRepository {
+        val userPreference = UserPreference.getInstance(context.dataStore)
+        val apiService = ApiClient.create(
+            runBlocking { userPreference.getUser().first().token }
+        )
+        return UserRepository(apiService)
     }
 }
