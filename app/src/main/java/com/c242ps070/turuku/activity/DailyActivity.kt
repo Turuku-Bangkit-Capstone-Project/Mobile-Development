de418e09fa7dbce2077a989e1224fe96e82471df
Rabsxd <reyanbz.rb@gmail.com>
2024-12-05 15:19:21 +0700

    adding UI activity_changepass.xml activity_daily.xmlty

--- app/src/main/java/com/c242ps070/turuku/activity/DailyActivity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/DailyActivity.kt
@@ -10,12 +10,7 @@ import com.c242ps070.turuku.R
 class DailyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_daily)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
--- app/src/main/java/com/c242ps070/turuku/dashboard_fragment/PreferencesFragment.kt
+++ app/src/main/java/com/c242ps070/turuku/dashboard_fragment/PreferencesFragment.kt
@@ -1,20 +1,49 @@
 package com.c242ps070.turuku.dashboard_fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.c242ps070.turuku.R
+import com.c242ps070.turuku.activity.ProfileActivity
+import com.c242ps070.turuku.activity.ChangepassActivity
+import com.c242ps070.turuku.activity.DailyActivity
+import com.c242ps070.turuku.databinding.FragmentPreferencesBinding
 
 class PreferencesFragment : Fragment() {
+    private var _binding: FragmentPreferencesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_preferences, container, false)
+        _binding = FragmentPreferencesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnProfile.setOnClickListener {
+            val intent = Intent(activity, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnDailyActivity.setOnClickListener {
+            val intent = Intent(activity, DailyActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnChangepass.setOnClickListener {
+            val intent = Intent(activity, ChangepassActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
