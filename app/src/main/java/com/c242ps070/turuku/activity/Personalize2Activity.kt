f3eb8e70960a403490d2556b4ea061273213e9d8
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-07 19:50:31 +0700

    add viewmodel, init gender dropdown, get age and gender and save age and gender to datastore

--- app/src/main/java/com/c242ps070/turuku/activity/Personalize2Activity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/Personalize2Activity.kt
@@ -2,20 +2,59 @@ package com.c242ps070.turuku.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.c242ps070.turuku.R
 import com.c242ps070.turuku.databinding.ActivityPersonalize2Binding
+import com.c242ps070.turuku.viewmodel.Personalize2ViewModel
+import com.c242ps070.turuku.viewmodel.factory.ViewModelFactory
+import com.google.android.material.textfield.MaterialAutoCompleteTextView
 
 class Personalize2Activity : AppCompatActivity() {
-
     private lateinit var binding : ActivityPersonalize2Binding
+    private lateinit var viewModel: Personalize2ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPersonalize2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViewModel()
+        initDropdown()
+
+        viewModel.getUserLoggedIn().observe(this) { user ->
+            if (user.age != null && user.gender != null) {
+                binding.inputAge.setText(String.format(user.age.toString()))
+                binding.inputGender.editText?.setText(user.gender)
+            }
+        }
 
         binding.btnContinue.setOnClickListener{
+            saveAgeAndGender()
+        }
+    }
+
+    private fun initViewModel() {
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, factory)[Personalize2ViewModel::class.java]
+    }
+
+    private fun initDropdown() {
+        val gender = resources.getStringArray(R.array.gender)
+        val adapter = ArrayAdapter(this, R.layout.dropdown_item, gender)
+        (binding.inputGender.editText as? MaterialAutoCompleteTextView)?.setAdapter(adapter)
+    }
+
+    private fun saveAgeAndGender() {
+        val age = binding.inputAge.text.toString()
+        val gender = binding.inputGender.editText?.text.toString()
+
+        if (age.isNotEmpty()) {
+            viewModel.saveAgeAndGender(age.toInt(), gender)
             startActivity(Intent(this, Personalize3Activity::class.java))
+            ViewModelFactory.clearInstance()
+        } else {
+            binding.inputAge.error = "Age is required"
         }
     }
 }
