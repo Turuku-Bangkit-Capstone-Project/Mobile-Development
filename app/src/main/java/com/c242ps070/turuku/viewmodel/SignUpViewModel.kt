d214fc975d50808c5ef86a624e36a4326e29cbe7
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-07 19:44:48 +0700

    add viewmodel for SignUpActivity

--- app/src/main/java/com/c242ps070/turuku/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/SignUpViewModel.kt
@@ -4,12 +4,12 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.remote.request.RegisterRequest
-import com.c242ps070.turuku.data.remote.response.RegisterResponse
+import com.c242ps070.turuku.data.remote.response.SuccessResponse
 import com.c242ps070.turuku.data.repository.AuthRepository
 
 class SignUpViewModel(
     private val authRepository: AuthRepository
 ): ViewModel() {
-    fun signUp(registerRequest: RegisterRequest): LiveData<Result<RegisterResponse>> =
+    fun signUp(registerRequest: RegisterRequest): LiveData<Result<SuccessResponse>> =
         authRepository.register(registerRequest)
 }
