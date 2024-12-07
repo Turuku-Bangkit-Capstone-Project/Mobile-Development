f23d2513ac72da84c72636448bc2f2eeb0ee8704
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-07 19:45:05 +0700

    add viewmodel for SplashScreenActivity

---
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/SplashScreenViewModel.kt
@@ -0,0 +1,13 @@
+package com.c242ps070.turuku.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.c242ps070.turuku.data.local.datastore.UserPreference
+import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
+
+class SplashScreenViewModel(
+    private val userPreference: UserPreference
+): ViewModel() {
+    fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
+}
