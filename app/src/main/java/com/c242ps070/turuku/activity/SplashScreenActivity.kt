7abfe3c74cb2fa3332a441185f05202e71e9882e
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-07 19:48:28 +0700

    update to add conditional what activity will started at the first time

--- app/src/main/java/com/c242ps070/turuku/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/SplashScreenActivity.kt
@@ -1,23 +1,45 @@
 package com.c242ps070.turuku.activity
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.c242ps070.turuku.R
+import androidx.lifecycle.ViewModelProvider
+import com.c242ps070.turuku.HomeActivity
+import com.c242ps070.turuku.databinding.ActivitySplashScreenBinding
+import com.c242ps070.turuku.viewmodel.SplashScreenViewModel
+import com.c242ps070.turuku.viewmodel.factory.ViewModelFactory
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    private lateinit var viewModel: SplashScreenViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViewModel()
+
+        viewModel.getUserLoggedIn().observe(this) { user ->
+            if (user.age != null && user.gender != null && user.bedTime != null && user.wakeupTime != null) {
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else if (user.token.isNotEmpty()) {
+                val intent = Intent(this, Personalize1Activity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
         }
+    }
+
+    private fun initViewModel() {
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, factory)[SplashScreenViewModel::class.java]
     }
 }
