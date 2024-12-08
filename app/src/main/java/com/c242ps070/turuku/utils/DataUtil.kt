84198a8e93365ca917fa5ef033ec9961cd8e5762
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-08 19:22:32 +0700

    add get sleep hour and get sleep duration

---
+++ app/src/main/java/com/c242ps070/turuku/utils/DataUtil.kt
@@ -0,0 +1,22 @@
+package com.c242ps070.turuku.utils
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+fun getSleepHour(bedTime: String): Int {
+    return bedTime.split(":")[0].toInt()
+}
+
+fun getSleepDuration(bedTime: String, wakeupTime: String): Long {
+    val format = SimpleDateFormat("HH:mm", Locale.getDefault())
+
+    val bedTimeDate = format.parse(bedTime)
+    val wakeupTimeDate = format.parse(wakeupTime)
+
+    var duration = wakeupTimeDate.time - bedTimeDate.time
+    if (duration < 0) {
+        duration += 24 * 60 * 60 * 1000
+    }
+
+    return duration / (60 * 60 * 1000)
+}
