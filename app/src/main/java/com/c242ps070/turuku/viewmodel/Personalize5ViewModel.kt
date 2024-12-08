4270e925c372e578c4cabb7d31c78751cf8cf78b
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-08 18:50:31 +0700

    remove get sleep recommendation and add get chronotype

--- app/src/main/java/com/c242ps070/turuku/viewmodel/Personalize5ViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/Personalize5ViewModel.kt
@@ -3,21 +3,27 @@ package com.c242ps070.turuku.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
-import com.c242ps070.turuku.data.remote.request.SleepRecommendationRequest
-import com.c242ps070.turuku.data.remote.response.SleepRecommendationResponse
+import com.c242ps070.turuku.data.remote.request.ChronotypeRequest
+import com.c242ps070.turuku.data.remote.response.ChronotypeResponse
 import com.c242ps070.turuku.data.repository.MachineLearningRepository
+import kotlinx.coroutines.launch
 
 class Personalize5ViewModel(
     private val machineLearningRepository: MachineLearningRepository,
     private val userPreference: UserPreference
 ): ViewModel() {
-    fun getSleepRecommendation(
-        request: SleepRecommendationRequest
-    ): LiveData<Result<SleepRecommendationResponse>> =
-        machineLearningRepository.getSleepRecommendation(request)
+    fun getChronotype(request: ChronotypeRequest): LiveData<Result<ChronotypeResponse>> =
+        machineLearningRepository.getChronotype(request)
 
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
+
+    fun saveChronotype(chronotype: String) {
+        viewModelScope.launch {
+            userPreference.saveChronotype(chronotype)
+        }
+    }
 }
