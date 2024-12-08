8e6213ca941a1c3e1e0c359a1a7203698328e768
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-08 18:37:24 +0700

    add machine learning repository in constructor

--- app/src/main/java/com/c242ps070/turuku/viewmodel/Personalize5ViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/Personalize5ViewModel.kt
@@ -3,11 +3,21 @@ package com.c242ps070.turuku.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
+import com.c242ps070.turuku.data.remote.request.SleepRecommendationRequest
+import com.c242ps070.turuku.data.remote.response.SleepRecommendationResponse
+import com.c242ps070.turuku.data.repository.MachineLearningRepository
 
 class Personalize5ViewModel(
+    private val machineLearningRepository: MachineLearningRepository,
     private val userPreference: UserPreference
 ): ViewModel() {
+    fun getSleepRecommendation(
+        request: SleepRecommendationRequest
+    ): LiveData<Result<SleepRecommendationResponse>> =
+        machineLearningRepository.getSleepRecommendation(request)
+
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
 }
