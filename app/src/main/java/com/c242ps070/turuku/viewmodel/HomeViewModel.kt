bb8412f31690e09dd6faa6c4a22b77bd2cd11bd7
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-09 10:35:57 +0700

    add viewmodel for HomeFragment

---
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
@@ -0,0 +1,24 @@
+package com.c242ps070.turuku.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.c242ps070.turuku.data.Result
+import com.c242ps070.turuku.data.local.datastore.UserPreference
+import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
+import com.c242ps070.turuku.data.remote.request.SleepRecommendationRequest
+import com.c242ps070.turuku.data.remote.response.SleepRecommendationResponse
+import com.c242ps070.turuku.data.repository.MachineLearningRepository
+import com.c242ps070.turuku.data.repository.UserRepository
+
+class HomeViewModel(
+    private val machineLearningRepository: MachineLearningRepository,
+    private val userRepository: UserRepository,
+    private val userPreference: UserPreference
+): ViewModel() {
+    fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
+
+    fun getSleepRecommendation(request: SleepRecommendationRequest)
+        : LiveData<Result<SleepRecommendationResponse>> =
+        machineLearningRepository.getSleepRecommendation(request)
+}
