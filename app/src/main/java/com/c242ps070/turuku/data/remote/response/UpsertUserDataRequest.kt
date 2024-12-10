e742a5ae3013ec5b8559d96634f6cab7fc0cce90
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-10 17:15:20 +0700

    remove id, bedTime and wakeupTime, add chronotype

--- app/src/main/java/com/c242ps070/turuku/data/remote/response/UpsertUserDataRequest.kt
+++ app/src/main/java/com/c242ps070/turuku/data/remote/response/UpsertUserDataRequest.kt
@@ -3,18 +3,12 @@ package com.c242ps070.turuku.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class UpsertUserDataRequest(
-    @SerializedName("id")
-    val id: Int? = null,
-
     @SerializedName("age")
     val age: Int? = null,
 
     @SerializedName("gender")
     val gender: String? = null,
 
-    @SerializedName("bedTime")
-    val bedTime: String? = null,
-
-    @SerializedName("wakeupTime")
-    val wakeupTime: String? = null
+    @SerializedName("chronotype")
+    val chronotype: String? = null
 )
