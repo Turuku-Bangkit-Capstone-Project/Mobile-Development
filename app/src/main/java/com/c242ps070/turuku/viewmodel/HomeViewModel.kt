78fb454edf36a5604459e8814d33477b8a86dfa4
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-10 22:45:35 +0700

    remove request for getSleepRecommendation

--- app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.asLiveData
 import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
-import com.c242ps070.turuku.data.remote.request.SleepRecommendationRequest
 import com.c242ps070.turuku.data.remote.response.SleepRecommendationResponse
 import com.c242ps070.turuku.data.repository.MachineLearningRepository
 import com.c242ps070.turuku.data.repository.UserRepository
@@ -18,7 +17,7 @@ class HomeViewModel(
 ): ViewModel() {
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
 
-    fun getSleepRecommendation(request: SleepRecommendationRequest)
+    fun getSleepRecommendation()
         : LiveData<Result<SleepRecommendationResponse>> =
-        machineLearningRepository.getSleepRecommendation(request)
+        machineLearningRepository.getSleepRecommendation()
 }
