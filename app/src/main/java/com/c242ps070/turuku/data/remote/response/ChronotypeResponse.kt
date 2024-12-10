c2b7ad0b8900582aeed0e2e9f2fa6ee830ab89b9
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-10 22:42:25 +0700

    remove success and change the message serialize name to msg

--- app/src/main/java/com/c242ps070/turuku/data/remote/response/ChronotypeResponse.kt
+++ app/src/main/java/com/c242ps070/turuku/data/remote/response/ChronotypeResponse.kt
@@ -3,10 +3,7 @@ package com.c242ps070.turuku.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class ChronotypeResponse(
-    @SerializedName("success")
-    val success: Boolean,
-
-    @SerializedName("message")
+    @SerializedName("msg")
     val message: String,
 
     @SerializedName("chronotype")
