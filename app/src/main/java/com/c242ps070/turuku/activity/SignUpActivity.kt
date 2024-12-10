616962d389b75ea6b9556715e513b0163e7d05cd
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-10 11:41:42 +0700

    add loading while sign up process

--- app/src/main/java/com/c242ps070/turuku/activity/SignUpActivity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/SignUpActivity.kt
@@ -2,6 +2,7 @@ package com.c242ps070.turuku.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.c242ps070.turuku.data.Result
@@ -37,34 +38,38 @@ class SignUpActivity : AppCompatActivity() {
 
         if (name.isNotEmpty() && email.isNotEmpty()
             && password.isNotEmpty() && confirmPassword.isNotEmpty()) {
-            if (confirmPassword == password) {
-                val registerRequest = RegisterRequest(
-                    name = name,
-                    email = email,
-                    password = password,
-                    confirmPassword = confirmPassword
-                )
-                viewModel.signUp(registerRequest).observe(this) { result ->
-                    if (result != null) {
-                        when (result) {
-                            is Result.Loading -> {
-                                //Loading
-                            }
-                            is Result.Success -> {
-                                val intent = Intent(this, LoginActivity::class.java)
-                                startActivity(intent)
-                            }
-                            is Result.Error -> {
-                                //Error
-                            }
+            val registerRequest = RegisterRequest(
+                name = name,
+                email = email,
+                password = password,
+                confirmPassword = confirmPassword
+            )
+            viewModel.signUp(registerRequest).observe(this) { result ->
+                if (result != null) {
+                    when (result) {
+                        is Result.Loading -> showLoading(true)
+                        is Result.Success -> {
+                            showLoading(false)
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        }
+                        is Result.Error -> {
+                            Toast.makeText(this@SignUpActivity, result.error, Toast.LENGTH_SHORT).show()
+                            showLoading(false)
                         }
                     }
                 }
-            } else {
-                // Password and confirm password do not match
             }
         } else {
-            // One or more fields are empty
+            Toast.makeText(this@SignUpActivity, "All fields are required", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun showLoading(isLoading: Boolean) {
+        binding.inputSignUpName.isEnabled = !isLoading
+        binding.inputSignUpEmail.isEnabled = !isLoading
+        binding.inputSignUpPassword.isEnabled = !isLoading
+        binding.inputSignUpConfirmPassword.isEnabled = !isLoading
+        binding.signUpButton.text = if (isLoading) "Signing up..." else "Sign Up"
     }
 }
