afc0e0148ecb438668d4871cf125bb1d94dde1b0
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-10 23:54:05 +0700

    remove success and sleep_duration

--- app/src/main/java/com/c242ps070/turuku/data/remote/response/SleepRecommendationResponse.kt
+++ app/src/main/java/com/c242ps070/turuku/data/remote/response/SleepRecommendationResponse.kt
@@ -3,14 +3,8 @@ package com.c242ps070.turuku.data.remote.response
 import com.google.gson.annotations.SerializedName
 
 data class SleepRecommendationResponse(
-    @SerializedName("success")
-    val success: Boolean,
-
-    @SerializedName("message")
+    @SerializedName("msg")
     val message: String,
-
-    @SerializedName("sleep_duration")
-    val sleepDuration: String,
 
     @SerializedName("recommended_sleep_duration")
     val recommendSleepDuration: String
