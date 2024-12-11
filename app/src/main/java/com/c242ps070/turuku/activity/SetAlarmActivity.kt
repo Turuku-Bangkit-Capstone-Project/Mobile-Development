678ac386239d20a4721ab3f39650c2a1e0eb0284
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-12 00:17:34 +0700

    add viewmodel, open time picker dialog when pick time button is click

--- app/src/main/java/com/c242ps070/turuku/activity/SetAlarmActivity.kt
+++ app/src/main/java/com/c242ps070/turuku/activity/SetAlarmActivity.kt
@@ -1,12 +1,67 @@
 package com.c242ps070.turuku.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.c242ps070.turuku.R
+import com.c242ps070.turuku.databinding.ActivitySetAlarmBinding
+import com.c242ps070.turuku.viewmodel.Personalize3ViewModel
+import com.c242ps070.turuku.viewmodel.SetAlarmViewModel
+import com.c242ps070.turuku.viewmodel.factory.ViewModelFactory
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.Calendar
+import java.util.Locale
 
 class SetAlarmActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySetAlarmBinding
+    private lateinit var viewModel: SetAlarmViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_alarm)
+        binding = ActivitySetAlarmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViewModel()
+
+        binding.btnPickTime.setOnClickListener {
+            openTimePickerDialog()
+        }
+
+        viewModel.wakeupTime.observe(this) { wakeupTime ->
+            binding.tvTime.text = wakeupTime
+            binding.btnPickTime.setText("Change time")
+        }
+    }
+
+    private fun initViewModel() {
+        val factory = ViewModelFactory.getInstance(this)
+        viewModel = ViewModelProvider(this, factory)[SetAlarmViewModel::class.java]
+    }
+
+    private fun openTimePickerDialog() {
+        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val currentMinute = Calendar.getInstance().get(Calendar.MINUTE)
+
+        val picker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(currentHour)
+                .setMinute(currentMinute)
+                .setTitleText("Select alarm time")
+                .build()
+
+        picker.addOnPositiveButtonClickListener {
+            val hour = picker.hour
+            val minute = picker.minute
+            val time = String.format(Locale.getDefault(), "%02d:%02d", hour, minute)
+
+            viewModel.setWakeupTime(time)
+            if (binding.tvTime.visibility == View.GONE) {
+                binding.tvTime.visibility = View.VISIBLE
+            }
+        }
+
+        picker.show(supportFragmentManager, "timePicker")
     }
 }
