0c8129753ce0caf64c5b821001031bda8ca36f33
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-11 16:09:24 +0700

    add get history

--- app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
@@ -8,12 +8,15 @@ import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
 import com.c242ps070.turuku.data.remote.request.LoginRequest
+import com.c242ps070.turuku.data.remote.response.HistoryResponse
 import com.c242ps070.turuku.data.remote.response.LoginResponse
 import com.c242ps070.turuku.data.repository.AuthRepository
+import com.c242ps070.turuku.data.repository.UserRepository
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
     private val authRepository: AuthRepository,
+    private val userRepository: UserRepository,
     private val userPreference: UserPreference
 ): ViewModel() {
     fun login(loginRequest: LoginRequest): LiveData<Result<LoginResponse>> =
@@ -26,4 +29,6 @@ class LoginViewModel(
     }
 
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
+
+    fun getHistory(): LiveData<Result<List<HistoryResponse>>> = userRepository.getHistory()
 }
