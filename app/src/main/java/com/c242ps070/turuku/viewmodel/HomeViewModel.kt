f9fbc9e62e536525491819db0bed993d5b3777c8
Rabsxd <reyanbz.rb@gmail.com>
2024-12-11 19:13:25 +0700

    update HomeViewModel.kt

--- app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
@@ -3,12 +3,15 @@ package com.c242ps070.turuku.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
 import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
 import com.c242ps070.turuku.data.remote.response.SleepRecommendationResponse
+import com.c242ps070.turuku.data.remote.response.UserDataResponse
 import com.c242ps070.turuku.data.repository.MachineLearningRepository
 import com.c242ps070.turuku.data.repository.UserRepository
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val machineLearningRepository: MachineLearningRepository,
@@ -17,7 +20,19 @@ class HomeViewModel(
 ): ViewModel() {
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
 
-    fun getSleepRecommendation()
-        : LiveData<Result<SleepRecommendationResponse>> =
+    fun getSleepRecommendation(): LiveData<Result<SleepRecommendationResponse>> =
         machineLearningRepository.getSleepRecommendation()
+
+    fun getUserData(): LiveData<Result<List<UserDataResponse>>> = userRepository.getUserData()
+
+    fun saveUserData(
+        age: Int,
+        gender: String,
+        chronotype: String
+    ) {
+        viewModelScope.launch {
+            userPreference.saveAgeAndGender(age, gender)
+            userPreference.saveChronotype(chronotype)
+        }
+    }
 }
