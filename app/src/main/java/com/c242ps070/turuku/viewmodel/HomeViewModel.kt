306799104eb3555229a3f12c6d74147467ed7900
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-11 23:45:18 +0700

    add get last history from room

--- app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/HomeViewModel.kt
@@ -7,8 +7,10 @@ import androidx.lifecycle.viewModelScope
 import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
+import com.c242ps070.turuku.data.local.room.entity.SleepHistoryEntity
 import com.c242ps070.turuku.data.remote.response.SleepRecommendationResponse
 import com.c242ps070.turuku.data.remote.response.UserDataResponse
+import com.c242ps070.turuku.data.repository.HistoryRepository
 import com.c242ps070.turuku.data.repository.MachineLearningRepository
 import com.c242ps070.turuku.data.repository.UserRepository
 import kotlinx.coroutines.launch
@@ -16,6 +18,7 @@ import kotlinx.coroutines.launch
 class HomeViewModel(
     private val machineLearningRepository: MachineLearningRepository,
     private val userRepository: UserRepository,
+    private val historyRepository: HistoryRepository,
     private val userPreference: UserPreference
 ): ViewModel() {
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
@@ -33,6 +36,15 @@ class HomeViewModel(
         viewModelScope.launch {
             userPreference.saveAgeAndGender(age, gender)
             userPreference.saveChronotype(chronotype)
+        }
+    }
+
+    fun getLastHistoryRoom(): LiveData<SleepHistoryEntity?> =
+        historyRepository.getLastSleepHistoryRoom()
+
+    fun updateSleepRecommendationRoom(id: Int, sleepRecommendation: String) {
+        viewModelScope.launch {
+            historyRepository.updateSleepRecommendationRoom(id, sleepRecommendation)
         }
     }
 }
