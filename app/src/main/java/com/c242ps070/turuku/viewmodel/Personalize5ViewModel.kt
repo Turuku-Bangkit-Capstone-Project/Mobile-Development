761fe00f032f31285fb3fd284188613a80726a80
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-11 19:34:14 +0700

    add history repository

--- app/src/main/java/com/c242ps070/turuku/viewmodel/Personalize5ViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/Personalize5ViewModel.kt
@@ -9,12 +9,12 @@ import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
 import com.c242ps070.turuku.data.remote.request.HistoryRequest
 import com.c242ps070.turuku.data.remote.response.SuccessResponse
-import com.c242ps070.turuku.data.repository.UserRepository
+import com.c242ps070.turuku.data.repository.HistoryRepository
 import kotlinx.coroutines.launch
 
 class Personalize5ViewModel(
-    private val userRepository: UserRepository,
-    private val userPreference: UserPreference
+    private val historyRepository: HistoryRepository,
+    private val userPreference: UserPreference,
 ): ViewModel() {
     fun getUserLoggedIn(): LiveData<UserPreferenceModel> = userPreference.getUser().asLiveData()
 
@@ -31,5 +31,5 @@ class Personalize5ViewModel(
     }
 
     fun addHistory(history: HistoryRequest): LiveData<Result<SuccessResponse>> =
-        userRepository.addHistory(history)
+        historyRepository.addHistory(history)
 }
