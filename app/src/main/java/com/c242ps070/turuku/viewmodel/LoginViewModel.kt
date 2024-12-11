334c46d354470b309f15b55c101bf4ecd6669d9d
Muhammad Helmy <muhhlmy@gmail.com>
2024-12-11 23:42:10 +0700

    replace save personalize with insert histories, remove user repository

--- app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/c242ps070/turuku/viewmodel/LoginViewModel.kt
@@ -7,17 +7,16 @@ import androidx.lifecycle.viewModelScope
 import com.c242ps070.turuku.data.Result
 import com.c242ps070.turuku.data.local.datastore.UserPreference
 import com.c242ps070.turuku.data.local.datastore.UserPreferenceModel
+import com.c242ps070.turuku.data.local.room.entity.SleepHistoryEntity
 import com.c242ps070.turuku.data.remote.request.LoginRequest
 import com.c242ps070.turuku.data.remote.response.HistoryResponse
 import com.c242ps070.turuku.data.remote.response.LoginResponse
 import com.c242ps070.turuku.data.repository.AuthRepository
 import com.c242ps070.turuku.data.repository.HistoryRepository
-import com.c242ps070.turuku.data.repository.UserRepository
 import kotlinx.coroutines.launch
 
 class LoginViewModel(
     private val authRepository: AuthRepository,
-    private val userRepository: UserRepository,
     private val historyRepository: HistoryRepository,
     private val userPreference: UserPreference,
 ): ViewModel() {
@@ -34,16 +33,9 @@ class LoginViewModel(
 
     fun getHistory(): LiveData<Result<List<HistoryResponse>>> = historyRepository.getHistory()
 
-    fun savePersonalize(
-        bedTime: String,
-        wakeupTime: String,
-        physicalActivity: Int,
-        dailySteps: Int
-    ) {
+    fun insertHistories(histories: List<SleepHistoryEntity>) {
         viewModelScope.launch {
-            userPreference.saveBedTime(bedTime)
-            userPreference.saveWakeupTime(wakeupTime)
-            userPreference.savePhysicalActivityAndDailySteps(physicalActivity, dailySteps)
+            historyRepository.insertAllSleepHistoryRoom(histories)
         }
     }
 }
